96283090f4b33921f59c51220b046e579dbe28a1
DocRedFo <docredfox@tmail1.net>
2019-12-29 13:57:01 +1000

    Initial commit

---
+++ app/src/main/java/com/wtf/image/MainActivity.java
@@ -0,0 +1,43 @@
+package com.wtf.image;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    ImageView view;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        view = findViewById(R.id.view);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DownloadImageTask(view).execute(editText.getText().toString());            }
+        });
+
+    }
+}
